841de6c174f8510c484944b37c10f35d1077a632
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2020-09-21 14:29:11 -0500

    working with elapsed visual, but gonna switch segments to rectangles per David A's suggestion

--- timeChunk.R
+++ timeChunk.R
@@ -1,5 +1,5 @@
 require(pacman)
-p_load(tidyverse,fs)
+p_load(tidyverse,fs,plotly,htmlwidgets)
 #source("https://raw.githubusercontent.com/galacticpolymath/ggGalactic/master/ggGalactic.R")
 
 #notRun
@@ -7,9 +7,10 @@ p_load(tidyverse,fs)
 
 destFolder<-"/Users/mattwilkins/GDrive/GP/Units/unit-FemalesSing/Lessons/Math/chunking"
 
-timeChunk <- function(totTime,segTimes,destFolder,prefix,Color,lineWidth){
+timeChunk <- function(totTime,segTimes,destFolder,prefix,Color,fadedColor,lineWidth){
   if(missing(Color)){Color="#2c83c3"}
-  if(missing(lineWidth)){lineWidth=3}
+  if(missing(fadedColor)){fadedColor="#1A4E75"}
+  if(missing(lineWidth)){lineWidth=7}
   if(missing(prefix)){prefix<-""}else{prefix<-paste0(prefix,"_")}
   starts<-c(0,cumsum(segTimes)[-length(segTimes)] )
   ends<-cumsum(segTimes)
@@ -20,26 +21,38 @@ timeChunk <- function(totTime,segTimes,destFolder,prefix,Color,lineWidth){
   for (i in 1:length(segTimes)){
     segTimes_i<-segTimes[i]
     df<-data.frame(x=seq(starts[i]+.5,ends[i]-.5,1))
-   
+    faded_x=data.frame(x=if(i==1){1}else{seq(0.5,ends[i-1]-.5,1)})
+    faded_col=ifelse(i==1,"transparent",fadedColor)
+    cust_breaks=seq(0,totTime,1)
+    cust_labels=rep("",totTime+1)
+    cust_labels[seq(1,totTime+1,5)]<-seq(0,totTime,5)
+    
   
   #plot
-  g<-ggplot(df)+geom_segment(aes(x=x,xend=x,y=0,yend=.5),colour=Color,lwd=lineWidth)+scale_x_continuous(breaks=seq(0,totTime,5),limits=c(-.5,totTime+.5))+xlab("")+ylab("")+geom_text(x=-1,y=.25,label=paste(segTimes_i,"min"),size=10,hjust=1,colour=Color)+ggGalactic()+coord_cartesian(clip="off",expand=F)+
+  g<-ggplot(df)+geom_segment(aes(x=x,xend=x,y=0,yend=.5),colour=Color,lwd=lineWidth,show.legend=F)+
+    geom_segment(inherit.aes=F,data=faded_x,aes(x=x,xend=x,y=0,yend=.15),colour=faded_col,lwd=lineWidth,show.legend=F)+scale_y_continuous(expand=c(0,0))+scale_x_continuous(breaks=cust_breaks,labels=cust_labels,limits=c(0,totTime+.5),expand=expansion(add=.6,mult=0))+xlab("")+ylab("")+ggGalactic()+
   theme(
     axis.text.y=element_blank(),
     axis.title=element_blank(),
+    axis.line.x=element_line(colour="#090816",size=1),
     axis.ticks.y=element_blank(),
-    axis.ticks.x = element_line(colour="gray30",size=.5),
+    axis.ticks.x = element_line(colour="#090816",size=.5),
+    axis.ticks.length=unit(6,"pt"),
     panel.grid.major.y=element_blank(),
     panel.grid.minor.y=element_blank(),
     panel.grid.minor.x=element_blank(),
+    panel.grid.major.x=element_blank(),
     panel.border=element_blank(),#element_rect(colour="gray50",size=.1),
-    panel.background = element_rect(fill="gray95"),
-    panel.grid.major.x=element_line(size=.5,colour="gray30"),
-    axis.text.x=element_text(colour="gray30"),
-    plot.margin=margin(c(5,10,-10,90),unit="pt")
+    panel.background = element_rect(fill="#f0f4ff"),
+    #panel.grid.major.x=element_line(size=.5,colour="#090816"),
+    axis.text.x=element_text(colour="#090816"),
+    plot.margin=margin(c(5,1,-10,-20),unit="pt")
   )
    
    ggsave(path(destFolder,paste0(prefix,"seg_",i,"of",length(segTimes)),ext="png"),plot=g,width=14,height=1)
+  # gPlotly<-ggplotly(g,width=290*2,height=21*10,staticPlot=T,tooltip = "none")
+   # saveWidget(as_widget(gPlotly),"timeChunk.html")
+   
    }#end for loop
   
   #lapply(1:length(Gs),function(i){
